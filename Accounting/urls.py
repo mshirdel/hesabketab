3709157e2a7581f41e6970e183a29e130d5869a7
meysam <meysam@localhost.localdomain>
2018-02-06 16:12:42 +0330

    new home page design and sign-up form

--- Accounting/urls.py
+++ Accounting/urls.py
@@ -3,6 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns=[
-    path('accounts/', include('django.contrib.auth.urls')),
     path('',views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/join', views.signup, name='sign_up'),
+    path('AboutUs/', views.index, name='about_us'),
+    path('ContactUs/', views.index, name='contact_us'),
+    path('blog/', views.index, name='blog'),
 ]
--- Accounting/views.py
+++ Accounting/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 def index(request):
     return render(request, 'Accounting/index.html')
+
+
+def signup(request):
+    # username = request.POST['username']
+    # email = request.POST['email']
+    # password = request.POST['password']
+    # data = {username: username, email: email, password: password}
+    return render(request, 'registration/join.html', { 'username': 'mshirdel' })
