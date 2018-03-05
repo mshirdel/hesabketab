65d78bfd7efbd28d2a23296be0b41a000821ea1e
meysam <meysam@localhost.localdomain>
2018-03-05 15:58:41 +0330

    add verbose name to models

--- Accounting/models.py
+++ Accounting/models.py
@@ -5,12 +5,21 @@ from django.contrib.auth.models import User
 class Group(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = "گروه"
+        verbose_name_plural = "گروه‌ها"
+
+    
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "تگ"
+        verbose_name_plural = "تگ‌ها"
 
     def __str__(self):
         return self.name
@@ -29,6 +38,10 @@ class Item(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
     item_type = models.CharField(max_length=3, choices=ITEM_TYPE)
+
+    class Meta:
+        verbose_name = "دخل و خرج"
+        verbose_name_plural = "دخل و خرج"
 
     def __str__(self):
         return f"{self.name} - {self.price}"
