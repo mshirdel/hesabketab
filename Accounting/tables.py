2be7ccc3302d267387f9f2bf69521338c9360f3a
Meysam Shirdel~ <mshirdel@gmail.com>
2018-12-05 11:43:51 +0330

    add seperator for price field

--- Accounting/tables.py
+++ Accounting/tables.py
@@ -1,5 +1,8 @@
+import re
+
 import django_tables2 as tables
 from django.utils.html import format_html
+# from django.contrib.humanize.templatetags.humanize import intcomma
 
 from .models import Item
 
@@ -10,9 +13,12 @@ class ItemTable(tables.Table):
     def render_commands(self, record):
         return format_html(f"<a href='items/{record.id}/update' class='btn btn-info btn-sm' >ویرایش</a>")
 
+    def render_price(self, value):
+        orig = str(value)
+        return re.sub(r"^(-?\d+)(\d{3})", r'\g<1>,\g<2>', orig)
+
     class Meta:
         model = Item
         exclude = ('created', 'modified', 'user')
-        # order_by = 'price'
-
+        per_page = 20
         template_name = 'django_tables2/bootstrap-responsive.html'
