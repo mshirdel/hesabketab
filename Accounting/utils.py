a7132f412911aa2daf64b223b0a10a1c4067c4f9
Meysam Shirdel~ <mshirdel@gmail.com>
2019-01-19 11:38:41 +0330

    change file import exception handling

--- Accounting/utils.py
+++ Accounting/utils.py
@@ -5,22 +5,22 @@ from .models import Item, Group
 
 
 def import_items(user, file_name='data.csv'):
-    try:
-        with open(file_name, newline='') as csvfile:
-            items = csv.reader(csvfile, delimiter=',')
-            new_items = []
-            for item in items:
-                new_items.append(Item(name=item[1],
-                                      price=item[2],
-                                      date=jdatetime.date(int(item[0].split(
-                                          '/')[0]), int(item[0].split('/')[1]), int(item[0].split('/')[2])),
-                                      item_type='Exp',
-                                      group=Group.objects.get(id=1),
-                                      user=user))
-            Item.objects.bulk_create(new_items)
-            return len(new_items)
-    except:
-        raise Exception("خطا در بارگزاری فایل csv")
+    # try:
+    with open(file_name, newline='') as csvfile:
+        items = csv.reader(csvfile, delimiter=',')
+        new_items = []
+        for item in items:
+            new_items.append(Item(name=item[1],
+                                    price=item[2],
+                                    date=jdatetime.date(int(item[0].split(
+                                        '/')[0]), int(item[0].split('/')[1]), int(item[0].split('/')[2])),
+                                    item_type='Exp',
+                                    group=Group.objects.get(id=1),
+                                    user=user))
+        Item.objects.bulk_create(new_items)
+        return len(new_items)
+    # except:
+    #     raise Exception("خطا در بارگزاری فایل csv")
 
 
 def convert_string_date_to_jdate(date):
